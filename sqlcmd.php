20c8806a9b56172ac25d3862cfa6b35077a2add3
Marc DUREN <marc.duren@mdsp.fr>
2024-11-06 20:55:05 +0100

    Bug boolean data

--- sqlcmd.php
+++ sqlcmd.php
@@ -3,152 +3,152 @@
      * auteur : Marc Duren
      */
 
-class sqlCmd
-{
-    public $m_fields = array();
-    public $m_values = array();
-    public $m_type = array(); //s=string,n=numeric,d=date,l=litteral,b=bool
-    protected $m_command;
-
-    protected function formated($n)
+    class sqlCmd
     {
-        $v = $this->m_values[$n];
-        $t = $this->m_type[$n];
-        switch ($t) {
-            case "n":
-                $v = str_replace(["\'","'"], "", $v); //pas de guillemets
-                if (strlen($v) === 0 || $v === "NaN") {
-                    return 'null';
-                }
-                return str_replace(',', '.', $v);
-            case "d":
-                $v = str_replace(["\'","'"], "", $v); //pas de guillemets
-                if (strlen($v) == 0) {
-                    return "null";
-                }
-                return "'" . $v . "'";
-            case "b":
-                $v = str_replace(["\'","'"], "", $v); //pas de guillemets
-                if (strlen($v) == 0) {
-                    return "null";
-                } else {
-                    if ($v == "t" || $v == "Oui" || $v == "on" || $v == "true" || $v == "TRUE") {
-                        return "TRUE";
+        public $m_fields = array();
+        public $m_values = array();
+        public $m_type = array(); //s=string,n=numeric,d=date,l=litteral,b=bool
+        protected $m_command;
+
+        protected function formated($n)
+        {
+            $v = $this->m_values[$n];
+            $t = $this->m_type[$n];
+            switch ($t) {
+                case "n":
+                    $v = str_replace(["\'","'"], "", $v); //pas de guillemets
+                    if (strlen($v) === 0 || $v === "NaN") {
+                        return 'null';
                     }
+                    return str_replace(',', '.', $v);
+                case "d":
+                    $v = str_replace(["\'","'"], "", $v); //pas de guillemets
+                    if (strlen($v) == 0) {
+                        return "null";
+                    }
+                    return "'" . $v . "'";
+                case "b":
+                    $v = str_replace(["\'","'"], "", $v); //pas de guillemets
+                    if (strlen($v) == 0) {
+                        return "null";
+                    } else {
+                        if ($v == "t" || $v == "Oui" || $v == "on" || $v == "true" || $v == "TRUE" || $v === "1") {
+                            return "TRUE";
+                        }
+                    }
+                    return "FALSE";
+                case "l":
+                    return $v;
+            }
+            return "'" . str_replace("'", "''", str_replace("\'", "'", $v)) . "'";
+        }
+
+        public function Add($tuple, $valeur, $type = "")
+        {
+            $this->m_fields[] = $tuple;
+            $this->m_values[] = $valeur;
+            if ($type == "") { //auto
+                if (is_numeric($valeur)) {
+                    $this->m_type[] = "n";
+                } else {
+                    $this->m_type[] = "s";
                 }
-                return "FALSE";
-            case "l":
-                return $v;
-        }
-        return "'" . str_replace("'", "''", str_replace("\'", "'", $v)) . "'";
-    }
-
-    public function Add($tuple, $valeur, $type = "")
-    {
-        $this->m_fields[] = $tuple;
-        $this->m_values[] = $valeur;
-        if ($type == "") { //auto
-            if (is_numeric($valeur)) {
-                $this->m_type[] = "n";
             } else {
-                $this->m_type[] = "s";
+                $this->m_type[] = $type;
             }
-        } else {
-            $this->m_type[] = $type;
-        }
-        return count($this->m_type) - 1;
-    }
-
-    public function AddNull($tuple)
-    {
-        $this->m_fields[] = $tuple;
-        $this->m_values[] = "null";
-        $this->m_type[] = "l";
-        return count($this->m_type) - 1;
-    }
-
-    public function MakeUpdateQuery($table, $sqlwhere)
-    {
-        $this->m_command = "UPDATE `$table` SET \n";
-        for ($n = 0; $n < count($this->m_fields); $n++) {
-            $f = $this->m_fields[$n];
-            if ($n > 0) {
-                $this->m_command .= ",\n";
-            }
-            $this->m_command .= "$f=" . $this->formated($n);
-        }
-        $this->m_command .= " \nWHERE $sqlwhere";
-        return $this->m_command;
-    }
-
-    public function MakeInsertQuery($table)
-    {
-        $this->m_command = "INSERT INTO `$table` (\n";
-        for ($n = 0; $n < count($this->m_fields); $n++) {
-            if ($n > 0) {
-                $this->m_command .= ",";
-            }
-            $this->m_command .= $this->m_fields[$n];
-        }
-        $this->m_command .= ") \nVALUES (";
-        for ($n = 0; $n < count($this->m_values); $n++) {
-            if ($n > 0) {
-                $this->m_command .= ",";
-            }
-            $this->m_command .= $this->formated($n);
+            return count($this->m_type) - 1;
         }
 
-        $this->m_command .= ")\n";
-        return $this->m_command;
+        public function AddNull($tuple)
+        {
+            $this->m_fields[] = $tuple;
+            $this->m_values[] = "null";
+            $this->m_type[] = "l";
+            return count($this->m_type) - 1;
+        }
+
+        public function MakeUpdateQuery($table, $sqlwhere)
+        {
+            $this->m_command = "UPDATE `$table` SET \n";
+            for ($n = 0; $n < count($this->m_fields); $n++) {
+                $f = $this->m_fields[$n];
+                if ($n > 0) {
+                    $this->m_command .= ",\n";
+                }
+                $this->m_command .= "$f=" . $this->formated($n);
+            }
+            $this->m_command .= " \nWHERE $sqlwhere";
+            return $this->m_command;
+        }
+
+        public function MakeInsertQuery($table)
+        {
+            $this->m_command = "INSERT INTO `$table` (\n";
+            for ($n = 0; $n < count($this->m_fields); $n++) {
+                if ($n > 0) {
+                    $this->m_command .= ",";
+                }
+                $this->m_command .= $this->m_fields[$n];
+            }
+            $this->m_command .= ") \nVALUES (";
+            for ($n = 0; $n < count($this->m_values); $n++) {
+                if ($n > 0) {
+                    $this->m_command .= ",";
+                }
+                $this->m_command .= $this->formated($n);
+            }
+
+            $this->m_command .= ")\n";
+            return $this->m_command;
+        }
+
+        public function Clear()
+        {
+            unset($this->m_fields);
+            unset($this->m_commands);
+            unset($this->m_type);
+            unset($this->m_command);
+            $this->m_fields = array();
+            $this->m_values = array();
+            $this->m_type = array();
+            $this->m_command = "";
+        }
+
+        public function GetSQL()
+        {
+            return $this->m_command;
+        }
     }
 
-    public function Clear()
+    interface sqlResult
     {
-        unset($this->m_fields);
-        unset($this->m_commands);
-        unset($this->m_type);
-        unset($this->m_command);
-        $this->m_fields = array();
-        $this->m_values = array();
-        $this->m_type = array();
-        $this->m_command = "";
+        public function sql_num_rows();
+        public function sql_fetch_array($r = null);
+        public function sql_fetch_assoc($r = null);
+        public function sql_fetch_all();
+        public function sql_num_fields();
+        public function sql_free_result();
     }
 
-    public function GetSQL()
+    interface sqlInterface
     {
-        return $this->m_command;
+        public function sql_connect($host, $user, $password, $database);
+        public function sql_error();
+        public function sql_close();
+        public function sql_query($query);
+        public function sql_result($query); //return sqlResult
+        public function sql_num_rows($result);
+        public function sql_fetch_array($result, $r = null);
+        public function sql_fetch_assoc($result, $r = null);
+        public function sql_fetch_all($result);
+        public function sql_fetch_result($result, $r, $field);
+        public function sql_num_fields($result);
+        public function sql_free_result($result);
+        public function sql_start_transaction();
+        public function sql_commit();
+        public function sql_rollback();
+        public function sql_insert_id();
     }
-}
-
-interface sqlResult
-{
-    public function sql_num_rows();
-    public function sql_fetch_array($r = null);
-    public function sql_fetch_assoc($r = null);
-    public function sql_fetch_all();
-    public function sql_num_fields();
-    public function sql_free_result();
-}
-
-interface sqlInterface
-{
-    public function sql_connect($host, $user, $password, $database);
-    public function sql_error();
-    public function sql_close();
-    public function sql_query($query);
-    public function sql_result($query); //return sqlResult
-    public function sql_num_rows($result);
-    public function sql_fetch_array($result, $r = null);
-    public function sql_fetch_assoc($result, $r = null);
-    public function sql_fetch_result($result, $r, $field);
-    public function sql_fetch_all($result);
-    public function sql_num_fields($result);
-    public function sql_free_result($result);
-    public function sql_start_transaction();
-    public function sql_commit();
-    public function sql_rollback();
-    public function sql_insert_id();
-}
 
 class mysqlResult implements sqlResult
 {
